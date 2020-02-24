89b2dfcd9352449c649896e078a688f9ad920f8b
David Kelley <techlab@Davids-MacBook-Pro.local>
2020-02-24 15:26:59 -0500

    added flash animation and audio

--- game.js
+++ game.js
@@ -10,6 +10,15 @@ function nextSequence() {
   var randomChosenColor = buttonColors[randomNumber];
   //Pushes the random color to the end of the gamePattern array
   gamePattern.push(randomChosenColor);
+
+  //jQuery flash animation that selects the id of the color based on the randomChosenColor variable.
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  //audio file is selected best based on randomChosenColor and is then played with .play() method
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
 
 nextSequence();
