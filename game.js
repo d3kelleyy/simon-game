5b012dc9517093be30eb5063942d75fa5f288295
David Kelley <techlab@Davids-MacBook-Pro.local>
2020-02-24 22:29:06 -0500

    Added user click audio and storing user clicks

--- game.js
+++ game.js
@@ -1,23 +1,28 @@
-//Array of colors
 var buttonColors = ["red", "blue", "green", "yellow"];
-//Empty array that will accept a random color that is pushed to it.
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+});
 
 function nextSequence() {
-  //Creates a random number between 0 and 3
   var randomNumber = Math.floor(Math.random() * 4);
-  //Variable that chooses a random color from the buttonColors array.
   var randomChosenColor = buttonColors[randomNumber];
-  //Pushes the random color to the end of the gamePattern array
   gamePattern.push(randomChosenColor);
 
-  //jQuery flash animation that selects the id of the color based on the randomChosenColor variable.
   $("#" + randomChosenColor)
+    .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
 
-  //audio file is selected best based on randomChosenColor and is then played with .play() method
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
